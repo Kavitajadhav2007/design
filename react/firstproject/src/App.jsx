41a4a892e10c3e04721ea07d578ab78005ba38cd
Kavitajadhav2007 <kj216934@gmail.com>
2026-02-26 18:38:07 +0530

    second project

--- react/firstproject/src/App.jsx
+++ react/firstproject/src/App.jsx
@@ -1,10 +1,11 @@
+import React from "react";
+
 function App() {
-  let age = 18;
   return (
-    <>
-      <h1>My age is {age}</h1>
-      This is my first project
-    </>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello React App 🚀</h1>
+      <p>This is my first simple project.</p>
+    </div>
   );
 }
 
